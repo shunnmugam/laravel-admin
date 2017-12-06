6bd536ca4b6cd6c78c5eee22cc6118a4b297ba45
shunmugam <shunramit@gmail.com>
2017-12-06 13:00:10 +0530

    Update Helper.php

--- src/Helper.php
+++ src/Helper.php
@@ -5,6 +5,12 @@
 function skin($url=false)
 {
     $skin =  asset('/'.'skin'.'/'.Cms::getCurrentTheme().'/');
+
+
+    if(!File::exists(asset('/'.'skin'.'/'.Cms::getCurrentTheme()))) {
+        $skin =  asset('/'.'skin'.'/theme1/');
+    }
+
     if($url)
         $skin.=$url;
     return $skin;
