84d169961d73e2aa05d31e58dd6de092085057aa
shunmugam <shunramit@gmail.com>
2017-12-23 12:53:15 +0530

    Update routes.php

--- src/stubs/cms/core/layout/routes.php
+++ src/stubs/cms/core/layout/routes.php
@@ -2,3 +2,20 @@
 Route::get('/',function(){
     return view('layout::site.welcome');
 })->name('home');
+/*assets*/
+Route::get('/assets/{module}/{type}/{file}', [ function ($module, $type, $file) {
+    $module = ucfirst($module);
+
+    $path = base_path("cms/local/".Cms::getCurrentTheme()."/$module/resources/asserts/$type/$file");
+
+    if (\File::exists($path)) {
+        //return response()->download($path, "$file");
+        if($type == 'js'){
+            return response()->file($path, array('Content-Type' => 'application/javascript'));
+        }else{
+            return response()->file($path, array('Content-Type' => 'text/css'));
+        }
+    }
+
+    return response()->json([ ], 404);
+}]);
